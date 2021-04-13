edbad08a46493852d974ff688e08d8f2f13dff3b
+ <git-commit-mcreasmith23@gmail.com>
2021-04-13 13:43:22 -0500

    frontend/src: added exact paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,28 @@ import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Home from './components/home';
+import connect from 'react-redux';
+import {getDishes} from './actions';
+import About from './components/about';
+import Index from './components/index';
+import Error from './components/error';
+import Form from './components/form';
 
 class App extends Component {
+
+  componentDidMount(){
+    this.props.getDishes()
+  }
+
   render(){
-    return(
+    if (this.props.loading){
+      return(
+        <h3>LOADING...</h3>
+      )
+    }
+  }
+
+  return(
       <Router>
         <Route exact path="/" component={Home}/>
         </Router>
