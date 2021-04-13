e13582cd5998a0a85380a646c3c076fef350c5ab
+ <git-commit-mcreasmith23@gmail.com>
2021-04-13 12:08:51 -0500

    frontend/src/components/form: created form

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -19,5 +19,31 @@ class Form extends Component{
     this.props.addDish(this.state, this.props.history)
   }
 
-  render()
+  render(){
+    return(
+      <div>
+        <h2>Add Dish</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name"> Name </label>
+            <input className="inputs" type="text"
+            id="name" name="name" value={this.state.name} onChange={this.handleChange}/>
+            <br />
+            <br />
+            <label htmlFor="meal_attributes"> Meal </label>
+            <input className="inputs" type="text" id="meal_attributes" name="meal_attributes" value={this.state.meal_attributes} onChange={this.handleChange}/>
+            <br />
+            <br />
+            <label htmlFor="notes"> Notes </label>
+            <textarea className="inputs" id="notes" name="notes" value={this.state.notes} onChange={this.handleChange}/>
+          </div>
+
+      <input className="button-color" type="submit" value="Add Dish"/>
+
+        </form>
+        </div>
+    )
+  }
 }
+
+export default connect (null, {addMeal})(Form)
