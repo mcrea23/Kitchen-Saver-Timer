f99e5df4f355844110aa21f1be86087255157d08
+ <git-commit-mcreasmith23@gmail.com>
2021-04-13 11:27:29 -0500

    frontend: Created about.js

---
+++ frontend/src/components/about.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+      <div> 
+        <h2>Welcome cowboy</h2>
+        <p>
+          Figure out whats below
+        </p>
+      </div>
+    )
+  }
+}
